02c286223df1448e97d39a3d0ecd22864217c751
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-08-02 12:46:38 -0400

    fix import show branch

--- status.py
+++ status.py
@@ -12,7 +12,7 @@ from __future__ import unicode_literals
 import requests
 from mo_json import value2json, json2value
 from mo_logs import Log
-from pyLibrary.queries import jx
+from jx_python import jx
 
 ACTIVEDATA = "http://activedata.allizom.org/query"
 
@@ -28,10 +28,10 @@ def status():
         "select": [
             {"name": "date", "value": "repo.changeset.date"},
             {"name": "task", "value": "task.id"},
-            {"name": "rev", "value": "repo.changeset.id12"}
+            {"name": "rev", "value": "repo.changeset.id12"},
+            {"name": "branch", "value": "repo.branch.name"}
         ],
         "where": {"and": [
-            # {"eq": {"repo.changeset.id12": "60a5308fa987"}},
             {"eq": {"treeherder.jobKind": "test"}},
             {"eq": {"build.type": "ccov"}},
             {"gt": {"action.start_time": {"date": "today-3day"}}}
@@ -39,10 +39,10 @@ def status():
         "limit": 10000,
         "format": "list"
     }))
-    coverage_runs = jx.sort(json2value(response.content.decode("utf8")).data, {"date": "desc"})
+    coverage_runs = jx.sort(json2value(response.content.decode("utf8")).data, [{"date": "desc"}, "branch"])
 
     # FOR EACH REVISION, GET STATS
-    for g, runs in jx.groupby(coverage_runs, "rev", contiguous=True):
+    for g, runs in jx.groupby(coverage_runs, ["rev", "branch"], contiguous=True):
         # find tasks in `coverage` table
         total_tasks = set(runs.task)
         response = requests.get(ACTIVEDATA, data=value2json({
@@ -53,7 +53,7 @@ def status():
             "limit": 10000
         }))
 
-        ingested_tasks = set(json2value(response.content.decode("utf8")).data.task)
+        ingested_tasks = set(json2value(response.content.decode("utf8")).data.task) - {None}
         task_rate = len(ingested_tasks) / len(total_tasks)
 
         files_processed = {}
@@ -64,29 +64,30 @@ def status():
         if ingested_tasks:
             response = requests.get(ACTIVEDATA, data=value2json({
                 "from": "coverage",
-                "edges": {"name":"file", "value":"source.file.name"},
+                "edges": {"name": "file", "value": "source.file.name"},
                 "where": {"eq": {"repo.changeset.id12": g.rev}},
                 "format": "list",
                 "limit": 100000
             }))
-            files_processed = set(json2value(response.content.decode("utf8")).data.file)
+            files_processed = set(json2value(response.content.decode("utf8")).data.file) - {None}
 
             # find files in the `coverage` table
             response = requests.get(ACTIVEDATA, data=value2json({
                 "from": "coverage-summary",
-                "edges": {"name":"file", "value":"source.file.name"},
+                "edges": {"name": "file", "value": "source.file.name"},
                 "where": {"eq": {"repo.changeset.id12": g.rev}},
                 "format": "list",
                 "limit": 100000
             }))
-            summary_files = set(json2value(response.content.decode("utf8")).data.file)
+            summary_files = set(json2value(response.content.decode("utf8")).data.file) - {None}
             if files_processed:
                 file_rate = len(summary_files) / len(files_processed)
             else:
                 file_rate = 0.0
 
         Log.note(
-            "{{rev}} - {{rate|percent}} DONE - {{date|datetime}} Tasks {{ingested_tasks}}/{{total_tasks}} ({{task_rate|percent}})  Files {{summary_files}}/{{files_processed}} ({{file_rate|percent}})",
+            "{{branch}}-{{rev}} - {{rate|percent}} DONE - {{date|datetime}} Tasks {{ingested_tasks}}/{{total_tasks}} ({{task_rate|percent}})  Files {{summary_files}}/{{files_processed}} ({{file_rate|percent}})",
+            branch=g.branch,
             rev=g.rev,
             date=runs[0].date,
             rate=task_rate * file_rate,
